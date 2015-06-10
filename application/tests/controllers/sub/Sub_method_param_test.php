139cc8e7f214c496847ecda9f4e4b30797243bc8
kenjis <kenji.uui@gmail.com>
2015-06-11 08:44:44 +0900

    Add test

--- application/tests/controllers/sub/Sub_method_param_test.php
+++ application/tests/controllers/sub/Sub_method_param_test.php
@@ -2,6 +2,12 @@
 
 class sub_Sub_method_param_test extends TestCase
 {
+	public function test_sub_method_param_index()
+	{
+		$output = $this->request('GET', ['Sub_method_param', 'index', 'abc', 'def']);
+		$this->assertEquals('param1: abc param2: def', $output);
+	}
+
 	public function test_uri_sub_sub_method_param_index()
 	{
 		$output = $this->request('GET', 'sub/sub_method_param/index/abc/def');
