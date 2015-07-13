5fec4edf8f3f66dc7795ac4510b874170df8d7f8
kenjis <kenji.uui@gmail.com>
2015-07-14 08:40:58 +0900

    Update controller tests with $this->request->setCallable()

--- application/tests/controllers/Mock_phpunit_test.php
+++ application/tests/controllers/Mock_phpunit_test.php
@@ -4,25 +4,13 @@ class Mock_phpunit_test extends TestCase
 {
 	public function test_send_okay()
 	{
-		$output = $this->request(
-			'POST',
-			['Mock_phpunit', 'send'],
-			[
-				'name' => 'Mike Smith',
-				'email' => 'mike@example.jp',
-				'body' => 'This is test mail.',
-			],
+		$this->request->setCallable(
 			function ($CI) {
 				$email = $this->getDouble('CI_Email', ['send' => TRUE]);
 				$this->verifyInvokedOnce($email, 'send');
 				$CI->email = $email;
 			}
 		);
-		$this->assertContains('Mail sent', $output);
-	}
-
-	public function test_send_error()
-	{
 		$output = $this->request(
 			'POST',
 			['Mock_phpunit', 'send'],
@@ -30,12 +18,28 @@ class Mock_phpunit_test extends TestCase
 				'name' => 'Mike Smith',
 				'email' => 'mike@example.jp',
 				'body' => 'This is test mail.',
-			],
+			]
+		);
+		$this->assertContains('Mail sent', $output);
+	}
+
+	public function test_send_error()
+	{
+		$this->request->setCallable(
 			function ($CI) {
 				$email = $this->getDouble('CI_Email', ['send' => FALSE]);
 				$this->verifyInvokedOnce($email, 'send');
 				$CI->email = $email;
 			}
+		);
+		$output = $this->request(
+			'POST',
+			['Mock_phpunit', 'send'],
+			[
+				'name' => 'Mike Smith',
+				'email' => 'mike@example.jp',
+				'body' => 'This is test mail.',
+			]
 		);
 		$this->assertContains('Error', $output);
 	}
