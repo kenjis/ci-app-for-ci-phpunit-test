9479c39d039f28d3d76e5d1acc9cc24a2edd2aff
kenjis <kenji.uui@gmail.com>
2015-07-19 09:53:01 +0900

    Add tests for verifyInvoked* methods

--- application/tests/controllers/Auth_test.php
+++ application/tests/controllers/Auth_test.php
@@ -2,14 +2,10 @@
 
 class Auth_test extends TestCase
 {
-	/**
-	 * @expectedException				PHPUnit_Framework_Exception
-	 * @expectedExceptionCode			0
-	 * @expectedExceptionMessageRegExp	!\ARedirect to .+/auth/login\z!
-	 */
 	public function test_index()
 	{
 		// This request is redirected to '/auth/login'
+		$this->setExpectedRedirect('auth/login');
 		$output = $this->request('GET', ['Auth', 'index']);
 	}
 
@@ -27,5 +23,49 @@ class Auth_test extends TestCase
 		);
 		$output = $this->request('GET', ['Auth', 'index']);
 		$this->assertContains('<p>Below is a list of the users.</p>', $output);
+	}
+
+	public function test_login_validation_fail()
+	{
+		$this->request->setCallable(
+			function ($CI) {
+				// Get mock object
+				$validation = $this->getDouble(
+					'CI_Form_validation',
+					['set_rules' => NULL, 'run' => FALSE, 'set_value' => 'foo']
+				);
+				// Verify invocations
+				$this->verifyInvokedMultipleTimes($validation, 'set_rules', 2);
+				$this->verifyInvokedOnce($validation, 'run');
+				$this->verifyNeverInvoked($validation, 'reset_validation');
+				$this->verifyInvoked($validation, 'set_value', ['identity']);
+				// Inject mock object
+				$CI->form_validation = $validation;
+			}
+		);
+		$output = $this->request('POST', 'auth/login');
+		$this->assertContains('<p>Please login with your email/username and password below.</p>', $output);
+	}
+
+	public function test_login_validation_pass_but_login_fail()
+	{
+		$this->request->setCallable(
+			function ($CI) {
+				// Get mock object
+				$validation = $this->getDouble(
+					'CI_Form_validation',
+					['set_rules' => NULL, 'run' => TRUE, 'set_value' => 'foo']
+				);
+				// Verify invocations
+				$this->verifyInvokedMultipleTimes($validation, 'set_rules', 2);
+				$this->verifyInvokedOnce($validation, 'run');
+				$this->verifyNeverInvoked($validation, 'set_value');
+				// Inject mock object
+				$CI->form_validation = $validation;
+			}
+		);
+		
+		$this->setExpectedRedirect('auth/login');
+		$output = $this->request('POST', 'auth/login');
 	}
 }
