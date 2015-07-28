1595e852f28c111b8f862b4ac7373c72ab29a565
kenjis <kenji.uui@gmail.com>
2015-07-28 12:56:11 +0900

    Update install.php

--- install.php
+++ install.php
@@ -23,10 +23,6 @@ class Installer
             'application/database'
         );
         self::recursiveCopy(
-            'vendor/kenjis/ci-phpunit-test/application/helpers',
-            'application/helpers'
-        );
-        self::recursiveCopy(
             'vendor/kenjis/ci-phpunit-test/application/libraries',
             'application/libraries'
         );
