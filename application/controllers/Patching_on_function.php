435ff236e74b2a3074d3f3d591d855e1d52769d1
kenjis <kenji.uui@gmail.com>
2015-08-01 11:52:50 +0900

    Add not working sample for function patching and passing by reference

--- application/controllers/Patching_on_function.php
+++ application/controllers/Patching_on_function.php
@@ -11,4 +11,12 @@ class Patching_on_function extends CI_Controller
 	{
 		echo mt_rand(1, 9);
 	}
+
+	public function passing_by_reference()
+	{
+		$count = 0;
+		echo preg_replace(array('/\d/', '/\s/'), '*', 'xp 4 to', -1 , $count);
+		echo "\n";
+		echo $count;
+	}
 }
--- application/tests/controllers/Patching_on_function_test.php
+++ application/tests/controllers/Patching_on_function_test.php
@@ -20,4 +20,21 @@ class Patching_on_function_test extends TestCase
 		$output = $this->request('GET', 'patching_on_function/another');
 		$this->assertContains('19', $output);
 	}
+
+	/**
+	 * This test does not work now
+	 */
+//	public function test_passing_by_reference_patch_on_preg_replace()
+//	{
+//		MonkeyPatch::patchFunction(
+//			'preg_replace',
+//			function($pattern, $replacement, $subject, $limit, &$count) {
+//				$count = 999;
+//				return 'Patch by Monkey Patching';
+//			}
+//		);
+//		$output = $this->request('GET', 'patching_on_function/passing_by_reference');
+//		$this->assertContains('Patch by Monkey Patching', $output);
+//		$this->assertContains('999', $output);
+//	}
 }
