0ab5c8d14909f26e9d21af94cbc150a528831852
kenjis <kenji.uui@gmail.com>
2015-08-29 10:26:51 +0900

    Install codeigniter-restserver 2.7.2

--- application/config/routes.php
+++ application/config/routes.php
@@ -51,6 +51,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
-$route['translate_uri_dashes'] = FALSE;
+$route['translate_uri_dashes'] = TRUE;
 
-$route['welcome/index'] = 'sub/sub/index';
+/*
+| -------------------------------------------------------------------------
+| Sample REST API Routes
+| -------------------------------------------------------------------------
+*/
+$route['api/example/users/(:num)'] = 'api/example/users/id/$1'; // Example 4
+$route['api/example/users/(:num)(\.)([a-zA-Z0-9_-]+)(.*)'] = 'api/example/users/id/$1/format/$3$4'; // Example 8
