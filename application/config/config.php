7b32c6f3ff0d8ad6a85ee0dc3f4790a5aa6ecfac
kenjis <kenji.uui@gmail.com>
2015-09-01 16:06:53 +0900

    Add script to run PHP built-in web server

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/';
+$config['base_url'] = 'http://localhost:8000/';
 
 /*
 |--------------------------------------------------------------------------
