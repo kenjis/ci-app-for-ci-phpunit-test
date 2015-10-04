8a25b331d50f735fa36da555af1e8d5c078b0b80
kenjis <kenji.uui@gmail.com>
2015-10-05 07:36:46 +0900

    Add tests for Backtrace

--- application/tests/_tests/patcher/Patcher/Backtrace_test.php
+++ application/tests/_tests/patcher/Patcher/Backtrace_test.php
@@ -21,6 +21,130 @@ class Backtrace_test extends \PHPUnit_Framework_TestCase
 		$this->assertNull($info['function']);
 	}
 
+	public function provide_PHP5_trace()
+	{
+		return array (
+			0 => 
+			array (
+				'file' => '/vagrant/debug_backtrace/Proxy.php',
+				'line' => 7,
+				'function' => 'checkCalledMethod',
+				'class' => 'Proxy',
+				'type' => '::',
+				'args' => 
+				array (
+					0 => 'bar',
+				),
+			),
+			1 => 
+			array (
+				'file' => '/vagrant/debug_backtrace/Test.php',
+				'line' => 7,
+				'function' => '__callStatic',
+				'class' => 'Proxy',
+				'type' => '::',
+				'args' => 
+				array (
+					0 => 'bar',
+					1 => 
+					array (
+					),
+				),
+			),
+			2 => 
+			array (
+				'file' => '/vagrant/debug_backtrace/Test.php',
+				'line' => 7,
+				'function' => 'bar',
+				'class' => 'Proxy',
+				'type' => '::',
+				'args' => 
+				array (
+				),
+			),
+			3 => 
+			array (
+				'file' => '/vagrant/debug_backtrace/app.php',
+				'line' => 7,
+				'function' => 'run',
+				'class' => 'Test',
+				'object' => 
+					new \stdClass,	// dummy
+				'type' => '->',
+				'args' => 
+				array (
+				),
+			),
+		);
+	}
+
+	public function provide_PHP7_trace()
+	{
+		return array (
+			0 => 
+			array (
+				'file' => '/vagrant/debug_backtrace/Proxy.php',
+				'line' => 7,
+				'function' => 'checkCalledMethod',
+				'class' => 'Proxy',
+				'type' => '::',
+				'args' => 
+				array (
+					0 => 'bar',
+				),
+			),
+			1 => 
+			array (
+				'file' => '/vagrant/debug_backtrace/Test.php',
+				'line' => 7,
+				'function' => '__callStatic',
+				'class' => 'Proxy',
+				'type' => '::',
+				'args' => 
+				array (
+					0 => 'bar',
+					1 => 
+					array (
+					),
+				),
+			),
+			2 => 
+			array (
+				'file' => '/vagrant/debug_backtrace/app.php',
+				'line' => 7,
+				'function' => 'run',
+				'class' => 'Test',
+				'object' => 
+					new \stdClass,	// dummy
+				'type' => '->',
+				'args' => 
+				array (
+				),
+			),
+		);
+	}
+
+	public function test_getInfo_FunctionPatcher_callStatic()
+	{
+		if (version_compare(PHP_VERSION, '6.0.0', '>='))
+		{
+			$trace = $this->provide_PHP7_trace();
+		}
+		else
+		{
+			$trace = $this->provide_PHP5_trace();
+		}
+
+		$info = Backtrace::getInfo('FunctionPatcher', $trace);
+
+		$this->assertEquals('Test', $info['class']);
+		$this->assertEquals('run', $info['method']);
+		$this->assertEquals(
+			'Test::run', $info['class_method']
+		);
+		$this->assertNull($info['function']);
+	}
+
 	public function test_getInfo_MethodPatcher()
 	{
 		$trace = debug_backtrace();
