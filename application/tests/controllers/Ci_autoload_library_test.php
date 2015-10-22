f8fba2a0f42d5d753a0d418eed07dd2002769b1c
kenjis <kenji.uui@gmail.com>
2015-10-23 06:55:53 +0900

    Fix test method name

--- application/tests/controllers/Ci_autoload_library_test.php
+++ application/tests/controllers/Ci_autoload_library_test.php
@@ -2,7 +2,7 @@
 
 class Ci_autoload_library_test extends TestCase
 {
-	public function test_index()
+	public function test_subdir()
 	{
 		$output = $this->request('GET', 'ci_autoload_library/subdir');
 		$this->assertEquals('Baz something', $output);
