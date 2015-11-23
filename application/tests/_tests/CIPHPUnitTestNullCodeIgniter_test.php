7ca5a3c1006cf77e6846e4402cf3ed9f1a5d5e10
kenjis <kenji.uui@gmail.com>
2015-11-23 09:47:15 +0900

    Add test for CIPHPUnitTestNullCodeIgniter

---
+++ application/tests/_tests/CIPHPUnitTestNullCodeIgniter_test.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @group ci-phpunit-test
+ */
+class CIPHPUnitTestNullCodeIgniter_test extends PHPUnit_Framework_TestCase
+{
+	/**
+	 * @expectedException LogicException
+	 * @expectedExceptionMessage CodeIgniter instance is not instantiated yet. You can't use `$this->load` at the moment. Please fix your test code.
+	 */
+	public function test_call()
+	{
+		$obj = new CIPHPUnitTestNullCodeIgniter();
+		$obj->load->libary('foo');
+	}
+}
