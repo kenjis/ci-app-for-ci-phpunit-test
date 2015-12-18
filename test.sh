03c52ecd466e91bf7b652a3dad50c07a26c2593a
kenjis <kenji.uui@gmail.com>
2015-12-19 07:20:00 +0900

    Improve test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
 
+XDEBUG=1	# 1 or 0
+
 cd `dirname $0`
+DIR=`pwd`
 
-cd application/tests
-../../vendor/bin/phpunit -v --debug $@
-#phpdbg -qrr ../../vendor/bin/phpunit -v --debug $@
+if [ -f "$DIR/vendor/bin/phpunit" ]; then
+	PHPUNIT="vendor/bin/phpunit"
+else
+	PHPUNIT=`which phpunit`
+fi
 
-#find . -name '*_test.php' -exec ../../vendor/bin/phpunit -v --debug {} \;
+if [ $XDEBUG -eq 1 ]; then
+	COMMAND="$PHPUNIT  -v --debug -c application/tests $@"
+else
+	COMMAND="phpdbg -qrr $PHPUNIT -v --debug -c application/tests $@"
+fi
+
+#echo $COMMAND; exit
+$COMMAND
+
+#find application/tests/ -name '*_test.php' -exec vendor/bin/phpunit -v --debug -c application/tests {} \;
