eb5ddf492b1fec4daf5dc78606c09f3273dabb6c
kenjis <kenji.uui@gmail.com>
2016-01-23 17:14:13 +0900

    Fix coding style

--- application/controllers/Set_header.php
+++ application/controllers/Set_header.php
@@ -2,8 +2,8 @@
 
 class Set_header extends CI_Controller
 {
-  public function get_request_header()
-  {
-    echo $this->input->get_request_header('X-Request-Header', TRUE);
-  }
+	public function get_request_header()
+	{
+		echo $this->input->get_request_header('X-Request-Header', TRUE);
+	}
 }
