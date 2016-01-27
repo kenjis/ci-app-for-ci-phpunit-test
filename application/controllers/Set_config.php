502458a939eecc7c7613db56f24de65af08130c1
kenjis <kenji.uui@gmail.com>
2016-01-27 15:06:20 +0900

    Fix coding style and remove unneeded file

--- application/controllers/Set_config.php
+++ application/controllers/Set_config.php
@@ -4,7 +4,7 @@ class Set_config extends CI_Controller
 {
 	public function config_item()
 	{
-    $config_key = $this->input->get('key');
+		$config_key = $this->input->get('key');
 		echo $this->config->item($config_key);
 	}
 }
