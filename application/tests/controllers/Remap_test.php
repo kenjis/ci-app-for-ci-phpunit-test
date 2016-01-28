68217566697ba67eb6b696aadc13718f1b3b5276
kenjis <kenji.uui@gmail.com>
2016-01-29 07:36:38 +0900

    Improve test case for _remap()

--- application/tests/controllers/Remap_test.php
+++ application/tests/controllers/Remap_test.php
@@ -4,28 +4,28 @@ class Remap_test extends TestCase
 {
 	public function test_index()
 	{
-		$output = $this->request('GET', ['Remap', '_remap', 'abc']);
-		$expected = 'index';
-		$this->assertEquals($expected, $output);
-	}
-
-	public function test_some_method()
-	{
-		$output = $this->request('GET', ['Remap', '_remap', 'some_method']);
-		$expected = 'another';
-		$this->assertEquals($expected, $output);
-	}
-
-	public function test_uri_remap_abc()
-	{
 		$output = $this->request('GET', 'remap/abc');
 		$expected = 'index';
 		$this->assertEquals($expected, $output);
 	}
 
-	public function test_uri_remap_some_method()
+	public function test_another()
 	{
 		$output = $this->request('GET', 'remap/some_method');
+		$expected = 'another';
+		$this->assertEquals($expected, $output);
+	}
+
+	public function test_remap_abc()
+	{
+		$output = $this->request('GET', ['Remap', '_remap', 'abc']);
+		$expected = 'index';
+		$this->assertEquals($expected, $output);
+	}
+
+	public function test_remap_some_method()
+	{
+		$output = $this->request('GET', ['Remap', '_remap', 'some_method']);
 		$expected = 'another';
 		$this->assertEquals($expected, $output);
 	}
