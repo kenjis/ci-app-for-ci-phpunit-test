7563b44470d20a23d4511edbf78e9980d092baa4
kenjis <kenji.uui@gmail.com>
2016-03-27 21:02:47 +0900

    Improve test case

--- application/controllers/Patching_on_constant.php
+++ application/controllers/Patching_on_constant.php
@@ -4,6 +4,17 @@ class Patching_on_constant extends CI_Controller
 {
 	public function index()
 	{
-		echo ENVIRONMENT;
+		if (ENVIRONMENT === 'production')
+		{
+			echo 'This is production enviromnent.';
+		}
+		elseif (ENVIRONMENT === 'development')
+		{
+			echo 'This is development enviromnent.';
+		}
+		elseif (ENVIRONMENT === 'testing')
+		{
+			echo 'This is testing enviromnent.';
+		}
 	}
 }
--- application/tests/controllers/Patching_on_constant_test.php
+++ application/tests/controllers/Patching_on_constant_test.php
@@ -5,17 +5,23 @@
  */
 class Patching_on_constant_test extends TestCase
 {
+	public function test_index()
+	{
+		$output = $this->request('GET', 'patching_on_constant');
+		$this->assertEquals('This is testing enviromnent.', $output);
+	}
+
 	public function test_index_development()
 	{
 		MonkeyPatch::patchConstant('ENVIRONMENT', 'development', 'Patching_on_constant');
 		$output = $this->request('GET', 'patching_on_constant');
-		$this->assertEquals('development', $output);
+		$this->assertEquals('This is development enviromnent.', $output);
 	}
 
 	public function test_index_production()
 	{
 		MonkeyPatch::patchConstant('ENVIRONMENT', 'production', 'Patching_on_constant');
 		$output = $this->request('GET', 'patching_on_constant');
-		$this->assertEquals('production', $output);
+		$this->assertEquals('This is production enviromnent.', $output);
 	}
 }
