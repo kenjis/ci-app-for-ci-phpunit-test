c4a6b79362ef6b9fdb74a108843e4e7e9a4bb29e
kenjis <kenji.uui@gmail.com>
2016-04-17 08:11:14 +0900

    Update controllers/Ajax_test.php

--- application/tests/controllers/Ajax_test.php
+++ application/tests/controllers/Ajax_test.php
@@ -4,21 +4,21 @@ class Ajax_test extends TestCase
 {
 	public function test_index_non_ajax_call()
 	{
-		$output = $this->request('GET', ['Ajax', 'index']);
+		$output = $this->request('GET', 'ajax/index');
 		$expected = 'Not Ajax request';
 		$this->assertEquals($expected, $output);
 	}
 
 	public function test_index_ajax_call()
 	{
-		$output = $this->ajaxRequest('GET', ['Ajax', 'index']);
+		$output = $this->ajaxRequest('GET', 'ajax/index');
 		$expected = 'Ajax request';
 		$this->assertEquals($expected, $output);
 	}
 
 	public function test_index_non_ajax_call_again()
 	{
-		$output = $this->request('GET', ['Ajax', 'index']);
+		$output = $this->request('GET', 'ajax/index');
 		$expected = 'Not Ajax request';
 		$this->assertEquals($expected, $output);
 	}
