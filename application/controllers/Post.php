6ca558b8a8dc0721b378fd6ce7b4b859a8084b3b
kenjis <kenji.uui@gmail.com>
2017-07-29 15:48:28 +0900

    Add test to post array
    See https://github.com/kenjis/ci-phpunit-test/issues/197

--- application/controllers/Post.php
+++ application/controllers/Post.php
@@ -8,8 +8,21 @@ class Post extends CI_Controller
 		{
 			show_404('Not Found');
 		}
-		
+
 		$name = $this->input->post('name');
 		echo html_escape($name) . ', you sent POST request!';
+	}
+
+	public function post_array()
+	{
+		if ($this->input->method() !== 'post')
+		{
+			show_404('Not Found');
+		}
+
+		ob_start();
+		var_export($this->input->post('categories'));
+		$array = ob_get_clean();
+		echo $array;
 	}
 }
--- application/tests/controllers/Post_test.php
+++ application/tests/controllers/Post_test.php
@@ -13,4 +13,17 @@ class Post_test extends TestCase
 		$output = $this->request('POST', 'post/index', ['name' => 'Mike']);
 		$this->assertEquals('Mike, you sent POST request!', $output);
 	}
+
+	public function test_post_array()
+	{
+		$output = $this->request(
+			'POST',
+			'post/post_array',
+			['categories' => ['324']]
+		);
+		$expected = "array (
+  0 => '324',
+)";
+		$this->assertEquals($expected, $output);
+	}
 }
