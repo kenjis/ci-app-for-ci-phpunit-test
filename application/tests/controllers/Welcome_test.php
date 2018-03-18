f9ff453e3fbdd0bf099ffc288f9543bdfe283f62
kenjis <kenji.uui@gmail.com>
2018-03-18 19:42:59 +0900

    test: stream safe realpath() alternatives

--- application/tests/controllers/Welcome_test.php
+++ application/tests/controllers/Welcome_test.php
@@ -25,7 +25,7 @@ class Welcome_test extends TestCase
 	public function test_APPPATH()
 	{
 		$actual = realpath(APPPATH);
-		$expected = realpath(__DIR__ . '/../..');
+		$expected = dirname(dirname(__DIR__)).'';
 		$this->assertEquals(
 			$expected,
 			$actual,
