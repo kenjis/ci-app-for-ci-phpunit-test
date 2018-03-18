35cbf904c4c44944624078860b156aa4a942ddc1
kenjis <kenji.uui@gmail.com>
2018-03-18 19:46:01 +0900

    test: add test which raises a warning in controller output

---
+++ application/controllers/Warning.php
@@ -0,0 +1,10 @@
+<?php
+
+class Warning extends CI_Controller
+{
+	public function index()
+	{
+		echo __METHOD__;
+		echo $undefined_variable;
+	}
+}
---
+++ application/tests/controllers/Warning_test.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Part of ci-phpunit-test
+ *
+ * @author     Kenji Suzuki <https://github.com/kenjis>
+ * @license    MIT License
+ * @copyright  2015 Kenji Suzuki
+ * @link       https://github.com/kenjis/ci-phpunit-test
+ */
+
+class Warning_test extends TestCase
+{
+	public function test_index()
+	{
+		// This test is for checking if ci-phpunit-test throws RuntimeException
+		// when there is a warning in a request output.
+		// In the real world, you just should fix your code that causes warning.
+
+		try {
+			// The controller raise a warning, and ci-phpunit-test turns it into RuntimeException
+			$output = $this->request('GET', 'warning/index');
+			$this->assertContains('Warning::index', $output);
+		}
+		catch (RuntimeException $e)
+		{
+			$this->assertRegExp('/Undefined variable: undefined_variable/', $e->getMessage());
+
+			while (ob_get_level() > 1)
+			{
+				ob_end_clean();
+			}
+		}
+	}
+}
