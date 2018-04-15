9ba72a9d949c779fd8762f86b2a339739d5b53ea
kenjis <kenji.uui@gmail.com>
2018-04-15 17:23:39 +0900

    test: to test `$this->request()` twice in one test method

---
+++ application/tests/controllers/Request_twice_test.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Part of ci-phpunit-test
+ *
+ * @author     Kenji Suzuki <https://github.com/kenjis>
+ * @license    MIT License
+ * @copyright  2015 Kenji Suzuki
+ * @link       https://github.com/kenjis/ci-phpunit-test
+ */
+
+class Request_twice_test extends TestCase
+{
+	public function test_index()
+	{
+		$output = $this->request('GET', 'welcome/index');
+		$this->assertContains('<title>Welcome to CodeIgniter</title>', $output);
+
+		$this->request('GET', 'welcome/method_not_exist');
+		$this->assertResponseCode(404);
+	}
+}
